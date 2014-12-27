5dbcd4a21b01c126d4bcb9f57f9662bf1b5fe2b6
Stickman <stocktonmanners@yahoo.ca>
2014-12-27 16:17:03 -0700

    test

--- bootstrap_subtheme/Gruntfile.js
+++ bootstrap_subtheme/Gruntfile.js
@@ -1,27 +1,44 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
-        watch: {
-            src: {
-                files: [‘**/*.scss', '**/*.php'],
-                tasks: ['compass:dev']
-            },
-           options: {
-                livereload: true,
-            },
-        },
-        compass: {
-            dev: {
-                options: {
-                    sassDir: 'custom-sass/scss',
-                    cssDir: 'css',
-                    imagesPath: 'assets/img',
-                    noLineComments: false,
-                    outputStyle: 'compressed'
-                }
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      css: {
+        files: [
+          '**/*.sass',
+          '**/*.scss'
+        ],
+        tasks: ['compass']
+      },
+      options: {
+         livereload: true,
+      },
+      js: {
+        files: [
+          'assets/js/*.js',
+          'Gruntfile.js'
+        ],
+        tasks: ['jshint']
+      }
+    },
+    compass: {
+        dev: {
+            options: {
+                sassDir: 'custom-sass/scss',
+                cssDir: 'css',
+                imagesPath: 'assets/img',
+                noLineComments: false,
+                outputStyle: 'compressed'
             }
         }
-    });
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    }
+  });
+
+  // Load the Grunt plugins.
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register the default tasks.
+  grunt.registerTask('default', ['watch']);
 };
